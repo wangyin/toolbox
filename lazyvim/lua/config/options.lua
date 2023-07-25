2de7e29d337e876986ce49808afa9236e370d847
Yin Wang <xwangyin@amazon.com>
2023-07-25 09:39:48 -0400

    Change default on relativenumber and wrap.

--- lazyvim/lua/config/options.lua
+++ lazyvim/lua/config/options.lua
@@ -3,3 +3,6 @@
 -- Add any additional options here
 vim.opt.listchars = "tab:▸ ,trail:·,nbsp:␣,extends:❯,precedes:❮" -- show symbols for whitespace
 vim.opt.scrolloff = 10 -- keep 20 lines above and below the cursor
+-- vim.opt.cursorline = false
+vim.opt.relativenumber = false
+vim.opt.wrap = true
