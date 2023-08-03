5675a8163c4e45a240080c650401ef29915a08c6
Yin Wang <xwangyin@amazon.com>
2023-08-02 21:07:15 -0400

    Take control of python debug config.

--- lazyvim/lua/plugins/dap.lua
+++ lazyvim/lua/plugins/dap.lua
@@ -23,11 +23,6 @@ end
 return {
   {
     "mfussenegger/nvim-dap",
-    dependencies = {
-      "mfussenegger/nvim-dap-python",
-      "theHamsta/nvim-dap-virtual-text"
-    },
-    lazy = true,
     keys = {
       { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = "Breakpoint Condition" },
       { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
@@ -84,189 +79,20 @@ return {
         desc = "Generate .vscode/launch.json",
       },
     },
-    config = function()
-      -- local dap = require("dap")
-      -- dap.adapters.python = {
-      --   type = "executable",
-      --   command = vim.fn.stdpath("data") .. "/mason/packages/debugpy/venv/bin/python3",
-      --   args = { "-m", "debugpy.adapter" },
-      -- }
-
+    opts = function(_, opts)
       require("nvim-dap-virtual-text").setup({
         -- virt_text_pos = 'eol'
         virt_text_pos = vim.fn.has 'nvim-0.10' == 1 and 'inline' or 'eol',
       })
 
-      require("dap-python").setup(get_debugpy())
       require("dap.ext.vscode").load_launchjs()
-
-      local dap_breakpoint_color = {
-        breakpoint = {
-          ctermbg = 0,
-          fg = "#993939",
-          bg = "#31353f",
-        },
-        logpoing = {
-          ctermbg = 0,
-          fg = "#61afef",
-          bg = "#31353f",
-        },
-        stopped = {
-          ctermbg = 0,
-          fg = "#98c379",
-          bg = "#31353f",
-        },
-      }
-
-      vim.api.nvim_set_hl(0, "DapBreakpoint", dap_breakpoint_color.breakpoint)
-      vim.api.nvim_set_hl(0, "DapLogPoint", dap_breakpoint_color.logpoing)
-      vim.api.nvim_set_hl(0, "DapStopped", dap_breakpoint_color.stopped)
-
-      local dap_breakpoint = {
-        error = {
-          text = "",
-          texthl = "DapBreakpoint",
-          linehl = "DapBreakpoint",
-          numhl = "DapBreakpoint",
-        },
-        condition = {
-          text = "ﳁ",
-          texthl = "DapBreakpoint",
-          linehl = "DapBreakpoint",
-          numhl = "DapBreakpoint",
-        },
-        rejected = {
-          text = "",
-          texthl = "DapBreakpint",
-          linehl = "DapBreakpoint",
-          numhl = "DapBreakpoint",
-        },
-        logpoint = {
-          text = "",
-          texthl = "DapLogPoint",
-          linehl = "DapLogPoint",
-          numhl = "DapLogPoint",
-        },
-        stopped = {
-          text = "",
-          texthl = "DapStopped",
-          linehl = "DapStopped",
-          numhl = "DapStopped",
-        },
-      }
-
-      vim.fn.sign_define("DapBreakpoint", dap_breakpoint.error)
-      vim.fn.sign_define("DapBreakpointCondition", dap_breakpoint.condition)
-      vim.fn.sign_define("DapBreakpointRejected", dap_breakpoint.rejected)
-      vim.fn.sign_define("DapLogPoint", dap_breakpoint.logpoint)
-      vim.fn.sign_define("DapStopped", dap_breakpoint.stopped)
     end,
   },
   {
-    "rcarriga/nvim-dap-ui",
-    dependencies = {
-      "mfussenegger/nvim-dap",
-    },
+    "mfussenegger/nvim-dap-python",
     config = function()
-      local present, dapui = pcall(require, "dapui")
-      if not present then
-        return
-      end
-
-      dapui.setup({
-        icons = { expanded = "▾", collapsed = "▸", current_frame = "▸" },
-        mappings = {
-          -- Use a table to apply multiple mappings
-          expand = { "<CR>", "<2-LeftMouse>" },
-          open = "o",
-          remove = "d",
-          edit = "e",
-          repl = "r",
-          toggle = "t",
-        },
-        -- Use this to override mappings for specific elements
-        element_mappings = {
-          -- Example:
-          -- stacks = {
-          --   open = "<CR>",
-          --   expand = "o",
-          -- }
-        },
-        -- Expand lines larger than the window
-        -- Requires >= 0.7
-        expand_lines = vim.fn.has("nvim-0.7") == 1,
-        -- Layouts define sections of the screen to place windows.
-        -- The position can be "left", "right", "top" or "bottom".
-        -- The size specifies the height/width depending on position. It can be an Int
-        -- or a Float. Integer specifies height/width directly (i.e. 20 lines/columns) while
-        -- Float value specifies percentage (i.e. 0.3 - 30% of available lines/columns)
-        -- Elements are the elements shown in the layout (in order).
-        -- Layouts are opened in order so that earlier layouts take priority in window sizing.
-        layouts = {
-          {
-            elements = {
-              -- Elements can be strings or table with id and size keys.
-              { id = "scopes", size = 0.25 },
-              "breakpoints",
-              "stacks",
-              "watches",
-            },
-            size = 40, -- 40 columns
-            position = "left",
-          },
-          {
-            elements = {
-              "repl",
-              "console",
-            },
-            size = 0.25, -- 25% of total lines
-            position = "bottom",
-          },
-        },
-        controls = {
-          -- Requires Neovim nightly (or 0.8 when released)
-          enabled = true,
-          -- Display controls in this element
-          element = "repl",
-          icons = {
-            pause = "",
-            play = "",
-            step_into = "",
-            step_over = "",
-            step_out = "",
-            step_back = "",
-            run_last = "↻",
-            terminate = "□",
-          },
-        },
-        floating = {
-          max_height = nil, -- These can be integers or a float between 0 and 1.
-          max_width = nil, -- Floats will be treated as percentage of your screen.
-          border = "single", -- Border style. Can be "single", "double" or "rounded"
-          mappings = {
-            close = { "q", "<Esc>" },
-          },
-        },
-        windows = { indent = 1 },
-        render = {
-          max_type_length = nil, -- Can be integer or nil.
-          max_value_lines = 100, -- Can be integer or nil.
-        },
-      })
-
-      require("dap").listeners.after.event_initialized["dapui_config"] = function()
-        dapui.open()
-      end
-      require("dap").listeners.before.disconnect["dapui_config"] = function()
-        dapui.close()
-      end
-      require("dap").listeners.before.event_terminated["dapui_config"] = function()
-        dapui.close()
-      end
-      require("dap").listeners.before.event_exited["dapui_config"] = function()
-        dapui.close()
-      end
-    end,
+      require("dap-python").setup(get_debugpy())
+    end
   },
   -- which key integration
   {
