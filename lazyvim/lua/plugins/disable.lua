69cebe8f924829b1ad3586fb6037e1fba699c52b
Yin Wang <xwangyin@amazon.com>
2023-10-18 18:50:56 +0000

    Disable not used Python plugins.

---
+++ lazyvim/lua/plugins/disable.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "nvim-neotest/neotest",
+    enabled = false
+  },
+  {
+    "nvim-neotest/neotest-python",
+    enabled = false
+  },
+  {
+    "linux-cultist/venv-selector.nvim",
+    enabled = false
+  }
+}
