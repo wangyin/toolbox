211d281bee3b9997e3bbb417e9a9db8d6cc35c77
Yin Wang <xwangyin@amazon.com>
2023-12-25 01:26:04 +0000

    Correct import aerial.

--- lazyvim/lua/config/lazy.lua
+++ lazyvim/lua/config/lazy.lua
@@ -18,7 +18,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.formatting.prettier"},
     { import = "lazyvim.plugins.extras.formatting.black" },
-    { import = "lazyvim.plugins.extras.coding.aerial" },
+    { import = "lazyvim.plugins.extras.editor.aerial" },
     -- import/override with your plugins
     { import = "plugins" },
   },
