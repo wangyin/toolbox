ca9ade6e9fdbbab6546a085c9c3bbe94cb4d9731
Yin Wang <xwangyin@amazon.com>
2024-02-13 17:28:10 -0500

    Fix typo in treesitter config.

--- lazyvim/lua/plugins/treesitter.lua
+++ lazyvim/lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
       highlight = {
         disable = function (_, buf)
           local max_filesize = 100 * 1024
-          local ok, stats = pcall(vim.leap.fs_stat, vim.api.nvim_buf_get_name(buf))
+          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
           if ok and stats and stats.size > max_filesize then
             print("Large file -- disable treesitter")
             return true
