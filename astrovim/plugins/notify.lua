640ff1cf7519517b9c33dd3211900f9cee982cb7
yin wang <yinwang@yins-MacBook-Air.local>
2024-02-25 16:54:13 -0500

    Add noice nvim.

---
+++ astrovim/plugins/notify.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      stages = "static",
+      render = "compact",
+      fps = 5,
+      level = 1,
+      timeout = 1000,
+    }
+  },
+  {
+    "folke/noice.nvim",
+    opts = function(_, config)
+      config.lsp = {
+        signature = {
+          enabled = false,
+        },
+      }
+      return config
+    end,
+  },
+}
