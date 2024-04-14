c5ca09164c9947c0f745889c07d1c9a8e49620e9
yin wang <yinwang@yins-MacBook-Air.local>
2024-04-13 21:28:22 -0400

    Disable treesitter for JSON.

--- lazyvim/lua/plugins/treesitter.lua
+++ lazyvim/lua/plugins/treesitter.lua
@@ -3,7 +3,11 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       highlight = {
-        disable = function (_, buf)
+        disable = function(lang, buf)
+          if vim.tbl_contains({ "json" }, lang) then
+            print("Json file -- disable treesitter")
+            return true
+          end
           local max_filesize = 100 * 1024
           local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
           if ok and stats and stats.size > max_filesize then
