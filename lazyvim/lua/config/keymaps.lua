ed1526e7916ccda7a56c22453761b1e589777a46
yin wang <yinwang@yins-MacBook-Air.local>
2024-04-13 21:20:12 -0400

    Map m to %.

--- lazyvim/lua/config/keymaps.lua
+++ lazyvim/lua/config/keymaps.lua
@@ -7,9 +7,14 @@ local opts = { silent = true }
 
 keymap("n", "0", "^", opts)
 keymap("n", "^", "0", opts)
-keymap("i", "jj", "<ESC>", opts)
-keymap("i", "jk", "<ESC>", opts)
-keymap("v", "J", ":move '>+1<CR>gv-gv", opts)
-keymap("v", "K", ":move '<-2<CR>gv-gv", opts)
 keymap("n", "<Char-0xAA>", ":w<CR>", opts)
 keymap("n", "<Char-0xAB>", function() require("mini.bufremove").wipeout(0, false) end, opts)
+keymap("n", "m", "%", opts)
+keymap("n", "<leader>fn", ":Telescope notify<CR>", opts)
+
+keymap("i", "jj", "<ESC>", opts)
+keymap("i", "jk", "<ESC>", opts)
+
+keymap("v", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("v", "K", ":move '<-2<CR>gv-gv", opts)
+keymap("v", "m", "%", opts)
